55363c84eb22c687a5a044750da218419302cc65
= <karfannacif@gmail.com>
2017-02-25 14:45:31 -0300

    Edit view files

--- angular-rails/freelancer-app/src/app/homepage/homepage.component.ts
+++ angular-rails/freelancer-app/src/app/homepage/homepage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
