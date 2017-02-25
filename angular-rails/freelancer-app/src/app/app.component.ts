daba7fa214f6af328119be3c6e18379e443d7e4d
= <karfannacif@gmail.com>
2017-02-24 21:18:55 -0300

    Create app component

--- angular-rails/freelancer-app/src/app/app.component.ts
+++ angular-rails/freelancer-app/src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  moduleId: module.id
 })
 export class AppComponent {
-  title = 'Freelancer Bootcamp Homepage';
+  title = 'Freelance Bootcamp Dashboard';
 }
