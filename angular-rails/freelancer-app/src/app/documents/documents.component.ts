f8df3c92d9966e641227511ddf2a66123e63ead4
= <karfannacif@gmail.com>
2017-02-26 12:00:10 -0300

    Creating Mock Data

--- angular-rails/freelancer-app/src/app/documents/documents.component.ts
+++ angular-rails/freelancer-app/src/app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
   moduleId: module.id,
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./documents.component.css']
 })
 export class DocumentsComponent{
+  
+  documents: Document[] = [
+    {
+      title: 'My First Doc', 
+      description: 'description lorem ipsum', 
+      file_url: 'http://google.com', 
+      image_url: 'http://google.com/img', 
+      update_at:'2016-01-01'
+    },
+    {
+      title: 'My Second Doc', 
+      description: 'description second lorem ipsum', 
+      file_url: 'http://google.com', 
+      image_url: 'http://google.com/img', 
+      update_at:'2016-01-01'
+    },
+    {
+      title: 'My Last Doc', 
+      description: 'description last lorem ipsum', 
+      file_url: 'http://google.com', 
+      image_url: 'http://google.com/img', 
+      update_at:'2016-01-01'
+    }
+  ];
 
   constructor() { }
 
