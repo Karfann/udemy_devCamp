d1b0b1d7d71bf4ecba33f25678a6e010df55b004
= <karfannacif@gmail.com>
2017-02-28 11:29:07 -0300

    Add list view of proposal

--- angular-rails/freelancer-app/src/app/proposal/proposal-list.component.ts
+++ angular-rails/freelancer-app/src/app/proposal/proposal-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
   moduleId: module.id,
@@ -7,6 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./proposal-list.component.css']
 })
 export class ProposalListComponent implements OnInit {
+
+  proposalOne: Proposal = new Proposal(
+    1, 'Abc Company', 'http://google.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com'
+  )
+  proposalTwo: Proposal = new Proposal(
+    2, 'Xz Company', 'http://google.com', 'Angular 2', 120, 100, 12, 'jordan@devcamp.com'
+  )
+  proposalThree: Proposal = new Proposal(
+    3, 'Other Company', 'http://google.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com'
+  )
+
+  proposals:  Proposal [] = [this.proposalOne, this.proposalTwo, this.proposalThree];
 
   constructor() { }
   
