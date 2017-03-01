af745c1b68c4d8d92130d24f9d849b36623f60c5
= <karfannacif@gmail.com>
2017-02-28 22:26:16 -0300

    Add proposal show component

---
+++ angular-rails/freelancer-app/src/app/proposal/proposal-show.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-proposal-show',
+  templateUrl: './proposal-show.component.html'
+})
+export class ProposalShowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
