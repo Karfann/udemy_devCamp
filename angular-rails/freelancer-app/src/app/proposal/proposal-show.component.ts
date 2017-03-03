e09b4ef245cba80c1a468e6f8bf1a2e6504ae3df
= <karfannacif@gmail.com>
2017-03-02 23:44:15 -0300

    Add reading params in proposal show

--- angular-rails/freelancer-app/src/app/proposal/proposal-show.component.ts
+++ angular-rails/freelancer-app/src/app/proposal/proposal-show.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Proposal } from './proposal';
 
 @Component({
   moduleId: module.id,
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './proposal-show.component.html'
 })
 export class ProposalShowComponent implements OnInit {
+  
+  id: number;
+  routeId: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.routeId = this.route.params.subscribe(
+      params => {
+        this.id = +params['id'];
+      }
+    )
   }
 
 }
