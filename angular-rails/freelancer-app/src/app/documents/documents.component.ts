a5f81dc2ae83924f341866beb7bcfea426d58eeb
= <karfannacif@gmail.com>
2017-03-07 23:25:33 -0300

    Add bootstrap card

--- angular-rails/freelancer-app/src/app/documents/documents.component.ts
+++ angular-rails/freelancer-app/src/app/documents/documents.component.ts
@@ -16,21 +16,21 @@ export class DocumentsComponent{
       title: 'My First Doc', 
       description: 'description lorem ipsum', 
       file_url: 'http://google.com', 
-      image_url: 'http://google.com/img', 
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg/1024px-Mistakes-to-avoid-when-hiring-freelancers.jpg', 
       update_at:'2016-01-01'
     },
     {
       title: 'My Second Doc', 
       description: 'description second lorem ipsum', 
       file_url: 'http://google.com', 
-      image_url: 'http://google.com/img', 
+      image_url: 'http://maxpixel.freegreatpicture.com/static/photo/1x/Business-Office-Workstation-Macbook-Air-Notebook-336369.jpg', 
       update_at:'2016-01-01'
     },
     {
       title: 'My Last Doc', 
       description: 'description last lorem ipsum', 
       file_url: 'http://google.com', 
-      image_url: 'http://google.com/img', 
+      image_url: 'http://maxpixel.freegreatpicture.com/static/photo/1x/Freelancer-Business-Computer-Office-Laptop-Work-625892.jpg',
       update_at:'2016-01-01'
     }
   ];
